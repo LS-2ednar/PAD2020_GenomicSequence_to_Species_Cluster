66b172af922267157e99a02b39e2c8dc2c75dbfe
Lukas Schaub <lukas_schaub@gmx.ch>
2020-10-25 20:47:24 +0100

    worked on allingment function --> needs improvment as of right now

--- P2.py
+++ P2.py
@@ -80,6 +80,51 @@ def find_mat_max_corr(matrix):
         
     return(n,m)
 
+def alline(matrix, tbmat, seq1, seq2):
+    #initialize returning order as1 and as2 
+    as1 = []
+    as2 = []
+    #initialize aseq1 and aseq2 which are alignet sequences
+    aseq1 = []
+    aseq2 = []
+    #get corrdinates from highest value in scoring matrix
+    start = find_mat_max_corr(matrix)
+    #start traceback
+    while tbmat[start[0]][start[1]] != 0.0:
+        as1.append(start[0]+1)
+        as2.append(start[1]+1)
+        try:
+            start = (tbmat[start[0]][start[1]][0],tbmat[start[0]][start[1]][1])
+        except:
+            try:
+                as1.append(start[0]+1)
+                as2.append(start[1]+1)
+            except:
+                break
+    #sort for ease of use
+    as1 = sorted(as1)
+    as2 = sorted(as2)
+    
+    #the following part needs some more refinement it seems that sequence two does
+    #does not behave as intendet!!!
+    
+    #allign dna
+    for n in as1:
+        if len(as1) == len(seq1):
+            if as1[n] == as1[n-1]:
+                print('-')
+            else:
+                print(seq1[n])
+    
+    # for m in as2:
+    #     if as2[m] == as2[m-1]:
+    #         print('-')
+    #     else:
+    #         print(seq2[m])
+    
+    
+    # print(aseq1, aseq2)
+
 def print_matrix(matrix):
     """
     Allows the print of matrices for easier visual interpretation
