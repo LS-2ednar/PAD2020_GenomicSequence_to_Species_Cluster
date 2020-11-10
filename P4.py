4d395e11e3af541a16af3a499360737c54111b62
Lukas Schaub <lukas_schaub@gmx.ch>
2020-11-10 19:06:48 +0100

    final Clustering done

--- P4.py
+++ P4.py
@@ -1,38 +1,70 @@
-def Cluster(dist_Matrix):
-    abc = 'abcdefghijklmnopqrstuvwxyz'
-    sequences = []
+import P1
+
+def Cluster(dist_Matrix, listOflabels = get_labels(ParseSeqFile('sequencefile.txt'))):
+    """
     
-    for i in range(len(dist_Matrix[0])-1):
-        sequences.append(abc[i])
-    print(sequences)
-    cluster = []
-    print(cluster)
-    i = 1
+
+    Parameters
+    ----------
+    dist_Matrix : list of list of floats 
+        A list of with the genetical difference between two DNA sequences.
+
+    Returns
+    -------
+        A strting with the clustered DNA Labels
+
+    """
     
-    for line in dist_Matrix:
-        if i == 1:
+    labels = listOflabels
+    
+    while len(dist_Matrix) != 1:
+        
+        #find location of minium in matrix
+        n,m = find_mat_min_but_0(dist_Matrix)
+        
+        #put togehter what needs to be together
+        labels[n] = (labels[n], labels[m])
+        labels.pop(m)
+        
+        #recalc of matrix
+        for element in range(len(dist_Matrix[0])):
+            dist_Matrix[1][element] = (dist_Matrix[1][element] + dist_Matrix[0][element])/2
+        
+        for line in range(len(dist_Matrix)):
+            dist_Matrix[line][1] = (dist_Matrix[line][1] + dist_Matrix[line][0]) /2 
+        
+        #edit matrix size
+        #remove row
+        dist_Matrix.pop(n)
+        #remove col
+        for line in dist_Matrix:
+            line.pop(m)
+        
+        #correct new 0,0 value
+        dist_Matrix[0][0] = 0
+        
+    cluster = '(%s, %s)' % (labels[0],labels[1])
+    return cluster
+        
+def find_mat_min_but_0(matrix):
+    
+    minInLine = []
+    n = 0
+    m = 0
+    #remove zeros for acual minmum location
+    for line in matrix:
+        i = 0
+        for element in line:
+            if element == 0:
+                line[i] = 999999
             i += 1
-            line = line[i:]
-            indice = line.index(min(line))
-            cluster.append('(a, %s' % sequences[indice])
-            sequences.pop()
-            sequences.pop(indice)
-        else:
-            try:
-                i += 1
-                line = line[i:]
-                indice = line.index(min(line))
-                print(indice)
-                cluster.append('%s)' % sequences[indice])
-                sequences.pop(indice)
-            except:
-                continue
-        #print(cluster)
-    return(cluster)
-
-
-#########################
-##Function not done jet##
-#########################
-
-
+    
+    #finding mimum
+    for line in matrix:
+        minInLine.append(min(line))
+        for element in minInLine:
+            while minInLine[n] != min(minInLine):
+                n += 1
+    while matrix[n][m] != min(matrix[n]):
+        m +=1   
+    return(n,m)
