d13deb1f885d593105694dfe27ff4f695c369ed5
Lukas Schaub <lukas_schaub@gmx.ch>
2020-11-10 19:06:16 +0100

    almost done implementation

--- P3.py
+++ P3.py
@@ -1,6 +1,19 @@
 import numpy as np
 
 def howmany_sequences(listOfTuples):
+    """
+    Determines the amount of DNA sequences which where aligned
+
+    Parameters
+    ----------
+    listOfTuples : A list of Tuples
+        list of tuples .
+
+    Returns
+    -------
+        The number of sequence.
+
+    """
     pairs = 0
     for n in listOfTuples:
         pairs += 1
@@ -10,6 +23,19 @@ def howmany_sequences(listOfTuples):
     return(k)
 
 def init_Dist_Matrix(length):
+    """
+    initialies a distance matrix containing notthing but 'X'
+
+    Parameters
+    ----------
+    length : A Integer
+        A integer of length n.
+
+    Returns
+    -------
+        Initialies a nXn dist matrix containing only zeros.
+
+    """
     dist_matrix = []
         
     while len(dist_matrix) < length:
@@ -19,6 +45,21 @@ def init_Dist_Matrix(length):
     return(dist_matrix)
 
 def calculate_distance(seq1,seq2):
+    """
+    
+
+    Parameters
+    ----------
+    seq1 : String
+        DNA Sequence 1.
+    seq2 : String
+        DNA Sequence 2.
+
+    Returns
+    -------
+        A caluclated and corrected distance value between seq1 and seq2. 
+
+    """
     mmcounter = 0 #mismatchcount
     for i in range(len(seq1)):
         if seq1[i] != seq2[i]:
@@ -27,7 +68,20 @@ def calculate_distance(seq1,seq2):
     pcorr = -1*(3/4)*np.log(1-(4/3*p))
     return(pcorr)
     
-def ComputeDistMatrix(dictWithKeysAsTuplesAndTuplesAsElements): 
+def ComputeDistMatrix(dictWithKeysAsTuplesAndTuplesAsElements):
+    """
+    
+
+    Parameters
+    ----------
+    dictWithKeysAsTuplesAndTuplesAsElements : TYPE
+        A Dict with Tuples as Key and Tuples as Elements.
+
+    Returns
+    -------
+        distMatrix which is a list of values
+
+    """
     try:
         matrixdim = howmany_sequences(dictWithKeysAsTuplesAndTuplesAsElements)
         distMatrix = init_Dist_Matrix(matrixdim)
